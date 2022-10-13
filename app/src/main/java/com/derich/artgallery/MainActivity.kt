4030f7daccc4497d6e8671295c24a20ef9ac7ea8
AlanDerich <alangitonga15@gmail.com>
2022-10-13 11:52:12 +0300

    This is a simple art gallery application. It displays some of my images and shows a description of the year the picture was taken and the photographer. It has 2 buttons that you can use to navigate to the next and previous button. It shows the fundamentals that I have learnt i.e, (Box, Column and Row composables), (Text, Image, Buttons), state hoisting, lambda expressions, e.t.c

---
+++ app/src/main/java/com/derich/artgallery/MainActivity.kt
@@ -0,0 +1,201 @@
+package com.derich.artgallery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.derich.artgallery.ui.theme.ArtGalleryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtGalleryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ArtGalleryUI()
+                }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun ArtGalleryUI() {
+    var imageId by remember {mutableStateOf(1)}
+    val imageResourceId = when(imageId) {
+        1 -> R.drawable.my_passport
+        2 -> R.drawable.painting_blank
+        3 -> R.drawable.painting_grafitti
+        4 -> R.drawable.on_top_of_a_car
+        5 -> R.drawable.painting_unedited
+        6 -> R.drawable.posing_with_mask
+        else -> R.drawable.sunset_field
+
+    }
+    val contentDescription = when(imageId) {
+        1 -> stringResource(R.string.passport_image_description)
+        2 -> stringResource(R.string.picture_of_me_painting_nothing_description)
+        3 -> stringResource(R.string.picture_of_me_painting_graffiti_description)
+        4 -> stringResource(R.string.chilling_on_top_of_car_description)
+        5 -> stringResource(R.string.unedited_picture_of_myself_description)
+        6 -> stringResource(R.string.posing_wearing_a_mask_description)
+        else -> stringResource(R.string.watching_sunset_description)
+    }
+    val photographer = when(imageId) {
+        1 -> stringResource(R.string.lawren_photography)
+        2 -> stringResource(R.string.alan_gitonga)
+        3 -> stringResource(R.string.alan_gitonga)
+        4 -> stringResource(R.string.alan_gitonga)
+        5 -> stringResource(R.string.lawren_photography)
+        6 -> stringResource(R.string.lawren_photography)
+        else -> stringResource(R.string.lawren_photography)
+    }
+    val yearTaken = when(imageId) {
+        1 -> stringResource(R.string.year_2019)
+        2 -> stringResource(R.string.year_2020)
+        3 -> stringResource(R.string.year_2019)
+        4 -> stringResource(R.string.year_2019)
+        5 -> stringResource(R.string.year_2020)
+        6 -> stringResource(R.string.year_2019)
+        else -> stringResource(R.string.year_2021)
+
+    }
+    val onClickPrev :(Int) -> Unit = {
+        if (imageId in 2..7){
+            imageId--
+        }
+    }
+    val onClickNext: (Int) -> Unit = {
+        if (imageId in 1..6) {
+            imageId++
+        }
+    }
+        Box (modifier = Modifier
+            .fillMaxSize()
+            .wrapContentWidth(Alignment.CenterHorizontally),
+            contentAlignment = Alignment.Center){
+
+            //spacer to add space between image and pic description
+//            Spacer(modifier = Modifier.height(24.dp))
+            //a column with the image description and year it was taken
+            ImageDescription(imageResourceId = imageResourceId,contentDescription = contentDescription, photographer = photographer, yearTaken = yearTaken)
+            //a row with the next and previous button
+//            Spacer(modifier = Modifier.height(24.dp))
+            Row(modifier = Modifier.fillMaxSize(), verticalAlignment = Alignment.Bottom) {
+                Button(onClick = {onClickPrev(imageId)},
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .weight(1.0f)) {
+                    Text(text = "Previous" )
+                }
+                Spacer(modifier = Modifier.width(24.dp))
+                Button(onClick = {onClickNext(imageId)},
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .weight(1.0f)) {
+                    Text(text = "Next")
+                }
+            }
+//            PrevAndNextButtons(imageId, onClickPrev = {imageId= it}, onClickNext = {imageId = it})
+        }
+
+}
+@Composable
+fun ImageDescription(imageResourceId: Int, contentDescription: String, photographer: String, yearTaken: String) {
+    Column(modifier = Modifier
+        .padding(24.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter= painterResource(imageResourceId),
+            contentDescription = contentDescription, modifier = Modifier.wrapContentHeight(Alignment.CenterVertically))
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(modifier = Modifier
+            .border(
+                border = BorderStroke(1.dp, color = Color.White),
+                shape = RectangleShape
+            )) {
+
+            Text(text = contentDescription,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.padding(4.dp,4.dp,4.dp,0.dp))
+            Text(text = buildAnnotatedString {
+                withStyle(ParagraphStyle(lineHeight = 16.sp)){
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)){
+                        append(photographer)
+                    }
+                    append(yearTaken)
+                }
+            },
+                fontSize = 16.sp,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.padding(4.dp,0.dp,4.dp,4.dp))
+        }
+
+    }
+}
+@Composable
+fun BoxTesting() {
+    Box {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Cyan))
+        Box(
+            Modifier
+                .matchParentSize()
+                .padding(top = 20.dp, bottom = 20.dp)
+                .background(Color.Yellow)
+        )
+        Box(
+            Modifier
+                .matchParentSize()
+                .padding(40.dp)
+                .background(Color.Magenta)
+        )
+        Box(
+            Modifier
+                .align(Alignment.Center)
+                .size(300.dp, 300.dp)
+                .background(Color.Green)
+        )
+        Box(
+            Modifier
+                .align(Alignment.TopStart)
+                .size(150.dp, 150.dp)
+                .background(Color.Red)
+        )
+        Box(
+            Modifier
+                .align(Alignment.BottomEnd)
+                .size(150.dp, 150.dp)
+                .background(Color.Blue)
+        )
+    }
+}
